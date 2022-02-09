e87a9ff832eb60a4caba059149e2aae5a2f54022
SolomonYolo <solomon258@gmail.com>
2022-02-09 17:53:39 +0530

    Testing Git multiple users

---
+++ BasicProjectSetup/src/test/java/testCases/TestingMultipleUsersGit.java
@@ -0,0 +1,14 @@
+package testCases;
+
+public class TestingMultipleUsersGit {
+	
+	public static void TL(){
+		System.out.println("TL CODe");
+	}
+
+	public static void main(String[] args) {
+
+		TL();
+	}
+
+}

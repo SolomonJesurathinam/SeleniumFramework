af70aa2fbd5f44b7b9a5622a74875fd940740d5e
SolomonYolo <solomon258@gmail.com>
2022-02-09 18:09:28 +0530

    Updated code

--- BasicProjectSetup/src/test/java/testCases/TestingMultipleUsersGit.java
+++ BasicProjectSetup/src/test/java/testCases/TestingMultipleUsersGit.java
@@ -6,8 +6,11 @@ public class TestingMultipleUsersGit {
 		System.out.println("TL Code");
 	}
 
+	//updated code
+	
 	public static void testUSer() {
 		System.out.println("Test User");
+		System.out.println("Updated code");
 	}
 	public static void main(String[] args) {
 

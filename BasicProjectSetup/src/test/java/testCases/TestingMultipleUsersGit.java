cce0fc631996860c91bb201c739338ea9a2337a3
SolomonYolo <solomon258@gmail.com>
2022-02-09 17:58:29 +0530

    Changes for Test user

--- BasicProjectSetup/src/test/java/testCases/TestingMultipleUsersGit.java
+++ BasicProjectSetup/src/test/java/testCases/TestingMultipleUsersGit.java
@@ -3,12 +3,16 @@ package testCases;
 public class TestingMultipleUsersGit {
 	
 	public static void TL(){
-		System.out.println("TL CODe");
+		System.out.println("TL Code");
 	}
 
+	public static void testUSer() {
+		System.out.println("Test User");
+	}
 	public static void main(String[] args) {
 
 		TL();
+		testUSer();
 	}
 
 }
